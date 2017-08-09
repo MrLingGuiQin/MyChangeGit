22b2d9e40b13c813c05c882316cb7ed14efb10a6
ling <824801022@qq.com>
2017-08-09 22:01:24 +0800

    日历列表的实现

--- app/src/main/java/com/linguiqing/mychanage/ui/rxjava/RxImageLoadActivity.java
+++ app/src/main/java/com/linguiqing/mychanage/ui/rxjava/RxImageLoadActivity.java
@@ -1,7 +1,6 @@
 package com.linguiqing.mychanage.ui.rxjava;
 
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.widget.Button;
 import android.widget.ImageView;
 
@@ -9,22 +8,12 @@ import com.jakewharton.rxbinding2.view.RxView;
 import com.linguiqing.mychanage.R;
 import com.linguiqing.mychanage.base.BaseActivity;
 import com.linguiqing.mychanage.ui.rxjava.rxImageLoad.RxImageLoad;
-import com.linguiqing.mychanage.util.LogUtil;
-
-import java.util.NoSuchElementException;
 
 import butterknife.BindView;
-import butterknife.ButterKnife;
-import io.reactivex.Observable;
-import io.reactivex.ObservableEmitter;
-import io.reactivex.ObservableOnSubscribe;
-import io.reactivex.Observer;
 import io.reactivex.annotations.NonNull;
 import io.reactivex.functions.Consumer;
-import io.reactivex.functions.Predicate;
 
 public class RxImageLoadActivity extends BaseActivity {
-
 
     @BindView(R.id.btn_rx_image_load)
     Button mBtnRxImageLoad;
@@ -42,65 +31,8 @@ public class RxImageLoadActivity extends BaseActivity {
         RxView.clicks(mBtnRxImageLoad).subscribe(new Consumer<Object>() {
             @Override
             public void accept(@NonNull Object o) throws Exception {
-
                 RxImageLoad.with(mContext).load(mUrl).into(mImageView);
-        }
+            }
         });
-
-//        Observable<String> memoryObservable = Observable.create(new ObservableOnSubscribe<String>() {
-//            @Override
-//            public void subscribe(@NonNull ObservableEmitter<String> e) throws Exception {
-//                e.onNext("");
-//                e.onComplete();
-//            }
-//        });
-//
-//        Observable<String> diskObservable = Observable.create(new ObservableOnSubscribe<String>() {
-//            @Override
-//            public void subscribe(@NonNull ObservableEmitter<String> e) throws Exception {
-//                e.onNext("disk");
-//                e.onComplete();
-//            }
-//        });
-//
-//
-//        Observable<String> netWorkObservable = Observable.create(new ObservableOnSubscribe<String>() {
-//            @Override
-//            public void subscribe(@NonNull ObservableEmitter<String> e) throws Exception {
-//                e.onNext("netWork");
-//                e.onComplete();
-//            }
-//        });
-//
-//        RxView.clicks(mBtnRxImageLoad).subscribe(new Consumer<Object>() {
-//            @Override
-//            public void accept(@NonNull Object o) throws Exception {
-//
-//                Observable.concat(memoryObservable, diskObservable, netWorkObservable)
-//                        .switchIfEmpty(new Observable<String>() {
-//                            @Override
-//                            protected void subscribeActual(Observer<? super String> observer) {
-//                                observer.onError(new NoSuchElementException());
-//                            }
-//                        })
-//                        .filter(new Predicate<String>() {
-//                            @Override
-//                            public boolean test(String s) throws Exception {
-//                                return !TextUtils.isEmpty(s);
-//                            }
-//                        }).firstElement().toObservable()
-//                        .subscribe(new Consumer<String>() {
-//                            @Override
-//                            public void accept(@NonNull String s) throws Exception {
-//                                LogUtil.d(s);
-//                            }
-//                        }, new Consumer<Throwable>() {
-//                            @Override
-//                            public void accept(@NonNull Throwable throwable) throws Exception {
-//                                LogUtil.d(throwable.toString());
-//                            }
-//                        });
-//            }
-//        });
     }
 }
